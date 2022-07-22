65be0a4cb787ddb4babf02edefe819ec21c57c60
Areefan <32266809+Areefan@users.noreply.github.com>
2022-07-22 15:15:02 +0700

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+        <div>
+            <h1>About</h1>
+            <p>This is the TodoList app v.1.0.0. It is port of a Ract crash course</p>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default About;
